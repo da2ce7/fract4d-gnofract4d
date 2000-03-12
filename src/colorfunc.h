7990b27674b3bd9631a640775870bcc370d74142
Edwin Young <edwin@bathysphere.org>
2000-03-12 22:21:23 +0000

    *** empty log message ***

---
+++ src/colorfunc.h
@@ -0,0 +1,15 @@
+#ifndef _COLORFUNC_H_
+#define _COLORFUNC_H_
+
+/* a colorfunc is a function object which takes the scratch space info & 
+ * iteration count and combines one or more base colors from the palette 
+ * to produce the final color
+ */
+
+#include "colorizer_public.h"
+
+
+
+
+
+#endif /* _COLORFUNC_H_ */
