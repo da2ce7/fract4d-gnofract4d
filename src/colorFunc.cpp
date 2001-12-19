1fd88896aeb06efbb5654f7201e95121d8202095
Edwin Young <edwin@bathysphere.org>
2001-12-19 06:31:24 +0000

    adding colorFunc files

---
+++ src/colorFunc.cpp
@@ -0,0 +1,99 @@
+/* Gnofract4D -- a little fractal generator-browser program
+ * Copyright (C) 1999-2001 Edwin Young
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "colorFunc.h"
+#include "iterFunc.h"
+
+#include <cstddef>
+#include <cmath>
+#include <iostream>
+
+// just returns the number of iterations, cast to a double
+class flat_colorFunc : public colorFunc {
+public:
+    double operator()(int iter, double *scratch) const
+        {
+            return (double) iter;
+        }
+
+#ifdef HAVE_GMP
+    double operator()(int iter, gmp::f *scratch) const
+        {
+            return (double) iter;
+        }
+#endif
+};
+
+// return the iteration count plus a fractional part which ranges
+// from 1 if the point "only just" escaped to 0 if it reached infinity
+// this smooths the color bands which would otherwise appear
+class cont_colorFunc : public colorFunc {
+public:
+    double operator()(int iter, double *scratch) const
+        {
+            return ((double) iter) + scratch[EJECT]/scratch[EJECT_VAL];
+        }
+
+#ifdef HAVE_GMP
+    double operator()(int iter, gmp::f *scratch) const
+        {
+            ((double) iter) + scratch[EJECT]/scratch[EJECT_VAL];
+        }
+#endif
+
+};
+
+// just return zero, whatever - used by default for the inside of the Mset
+class zero_colorFunc : public colorFunc {
+public:
+    double operator()(int iter, double *scratch) const
+        {
+            return (double) 0.0;
+        }
+
+#ifdef HAVE_GMP
+    double operator()(int iter, gmp::f *scratch) const
+        {
+            return (double) 0.0;
+        }
+#endif
+
+};
+
+colorFunc *colorFunc_new(e_colorFunc e)
+{
+    colorFunc *pcf=NULL;
+    switch(e){
+    case COLORFUNC_FLAT:
+        pcf = new flat_colorFunc;
+        break;
+    case COLORFUNC_CONT:
+        pcf = new cont_colorFunc;
+        break;
+    case COLORFUNC_ZERO:
+        pcf = new zero_colorFunc;
+        break;
+    default:
+        std::cerr << "Warning: unknown colorFunc value" << (int)e << "\n";
+    }
+     
+    return pcf;
+}
+
---
+++ src/colorFunc.h
@@ -0,0 +1,50 @@
+/* Gnofract4D -- a little fractal generator-browser program
+ * Copyright (C) 1999-2001 Edwin Young
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+/* A colorFunc defines a mapping from the state of the computation
+once the iterations have been completed, to a floating-point number
+which is used to determine the distance into the colormap to use for
+drawing */
+
+#ifndef _COLORFUNC_H_
+#define _COLORFUNC_H_
+
+#include "colorizer_public.h"
+#include "calc.h"
+#include "pointFunc_public.h"
+
+#include <iosfwd>
+#include <string>
+
+// abstract base class
+class colorFunc {
+ public:
+    virtual double operator()(int iter, double *scratch) const = 0;
+#ifdef HAVE_GMP
+    virtual double operator()(int iter, gmp::f *scratch) const = 0;
+#endif
+};
+
+// factory method to construct bailout function objects 
+colorFunc *colorFunc_new(e_colorFunc);
+colorFunc *colorFunc_read(std::istream& s);
+
+
+#endif /*_COLORFUNC_H_*/
