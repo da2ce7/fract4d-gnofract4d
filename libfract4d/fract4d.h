3faf74c4178cc743f8853039df6822900cd73828
Edwin Young <edwin@bathysphere.org>
2003-03-28 03:03:58 +0000

    added some files missing from CVS

---
+++ libfract4d/fract4d.h
@@ -0,0 +1,13 @@
+/* The public interface to the library */
+
+#ifndef FRACT4D_H_
+#define FRACT4D_H_
+
+#include "fract_public.h"
+#include "image_public.h"
+#include "colorizer_public.h"
+#include "pointFunc_public.h"
+//#include "iterFunc.h"
+
+#endif /* FRACT4D_H_ */
+
