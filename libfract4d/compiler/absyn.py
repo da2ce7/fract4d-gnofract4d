b210abbc414277d305c0ee79c2b017832dfa240f
Edwin Young <edwin@bathysphere.org>
2003-05-28 03:47:11 +0000

    converted parser to create an absyn tree

---
+++ libfract4d/compiler/absyn.py
@@ -0,0 +1,31 @@
+# Abstract Syntax Tree produced by parser
+
+class Node:
+    def __init__(self,type,children=None,leaf=None):
+         self.type = type
+         if children:
+              self.children = children
+         else:
+              self.children = [ ]
+         self.leaf = leaf
+         
+    def __str__(self):
+        return "[%s : %s]" % (self.type , self.leaf)
+    
+    def pretty(self,depth=0):
+        str = " " * depth + "[%s : %s" % (self.type , self.leaf)
+        if self.children:
+            str += "\n"
+            for child in self.children:
+               str += child.pretty(depth+1) + "\n"
+            str += " " * depth + "]\n"
+        else:
+            str += "]"
+        return str
+
+# shorthand named ctors for specific node types
+def Number(n):
+    return Node("const", None, n)
+
+def Binop(op, left, right):
+    return Node("binop", [left, right], op)
