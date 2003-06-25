7239f7231b0e1a8a9828d67da210e9b1a6e63d3e
Edwin Young <edwin@bathysphere.org>
2003-06-25 05:15:45 +0000

    a bunch of lexer fixes and test cases

--- libfract4d/compiler/test-fractlexer.py
+++ libfract4d/compiler/test-fractlexer.py
@@ -4,7 +4,8 @@ import fractlexer
 class LexerTest(unittest.TestCase):
     def setUp(self):
         self.lexer = fractlexer.lexer
-
+        self.lexer.lineno = 1
+        
     # utility function - open file f, lex it, and return a list of
     # all the tokens therein
     
@@ -21,6 +22,7 @@ class LexerTest(unittest.TestCase):
         while 1:
             tok = self.lexer.token()
             if not tok: break      # No more input
+            #print tok
             toklist.append(tok)
 
         return toklist
@@ -42,12 +44,51 @@ loop:
 bailout:
    |z|>@bailout
 default:
-   title = "foo;bar\
+   title = "foo;bar\\
    baz"
 }
 ''')
         self.failUnless(tokens[0].type == tokens[1].type == "NEWLINE","first 2 should be newlines")
-            
+
+        str = filter(lambda tok : tok.type == "STRING", tokens)
+        self.failUnless(len(str) == 1 and str[0].value == "foo;barbaz", "string literal parsing problem")
+
+        sections = filter(lambda tok : tok.type == "SECT_STM", tokens)
+        self.assertEqual(len(sections),3, "wrong number of sections")
+        self.assertEqual(sections[0].lineno, 4, "line counting wrong")
+        self.assertEqual(sections[2].lineno, 10, "line counting wrong")
+
+    def testBadChars(self):
+        tokens = self.tokensFromString("$ hello ~\n ` ' goodbye")
+        self.failUnless(tokens[0].type == "error" and tokens[0].value == "$")
+        self.failUnless(tokens[4].type == "error" and
+                        tokens[4].value == "`" and
+                        tokens[4].lineno == 2)
+
+    def testFormIDs(self):
+        tokens = self.tokensFromString('''
+=05 { }
+0008 { }
+-fred- { }
+''')
+        for token in tokens:
+            self.failUnless(token.type == "FORM_ID" or
+                            token.type == "FORM_END" or
+                            token.type == "NEWLINE")
+
+    def testCommentFormula(self):
+        tokens = self.tokensFromString('''
+;Comment {
+
+@#$%554""}
+myComment {}
+''')
+        tokens = filter(lambda tok : tok.type != "NEWLINE", tokens)
+        print tokens
+        self.failUnless(tokens[0].type == "FORM_ID" and
+                        tokens[0].value == "myComment" and
+                        tokens[0].lineno == 5)
+        
 def suite():
     return unittest.makeSuite(LexerTest,'test')
 
