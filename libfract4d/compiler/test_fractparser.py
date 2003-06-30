609e27550615f46bb0f42857af49b52a26320155
Edwin Young <edwin@bathysphere.org>
2003-06-30 04:24:55 +0000

    some error-handling test cases

--- libfract4d/compiler/test_fractparser.py
+++ libfract4d/compiler/test_fractparser.py
@@ -10,30 +10,34 @@ class ParserTest(unittest.TestCase):
         fractlexer.lexer.lineno = 1
         #self.parser.lexer.lineno = 1
 
+    def parse(self,s):
+        fractlexer.lexer.lineno = 1
+        return self.parser.parse(s)
+    
     def tearDown(self):
         self.parser.restart()
         
     def testEmpty(self):
-        tree = self.parser.parse("\n")
+        tree = self.parse("\n")
         self.failUnless(absyn.CheckTree(tree))
         #print tree.pretty()
 
     def testEmptyFormula(self):
-        tree = self.parser.parse("t1 {\n}\n")
+        tree = self.parse("t1 {\n}\n")
         self.failUnless(absyn.CheckTree(tree))
         formula = tree.children[0]
         self.failUnless(formula.type == "formula" and formula.leaf == "t1")
         #print tree.pretty()
 
     def testErrorThenFormula(self):
-        tree = self.parser.parse("gibberish~\nt1 {\n}\n")
+        tree = self.parse("gibberish~\nt1 {\n}\n")
         self.failUnless(absyn.CheckTree(tree))
         #print tree.pretty()
         formula = tree.children[0]
         self.failUnless(formula.type == "formula" and formula.leaf == "t1")
 
     def testErrorInFormula(self):
-        tree = self.parser.parse("t1 {\n ~\n}\n")
+        tree = self.parse("t1 {\n ~\n}\n")
         self.failUnless(absyn.CheckTree(tree))
         #print tree.pretty()
         formula = tree.children[0]
@@ -60,9 +64,12 @@ class ParserTest(unittest.TestCase):
         self.assertParsesEqual(
             "a + b + c - d - e",
             "(((a+b)+c)-d)-e")
+        self.assertParsesEqual(
+            "a^b^c",
+            "a^(b^c)")
 
     def testSimpleMandelbrot(self):
-        t1 = self.parser.parse('''
+        t1 = self.parse('''
 MyMandelbrot {
 init:
     z = 0
@@ -76,14 +83,33 @@ default:
 ''')
         self.assertIsValidParse(t1)
 
+    def testParseErrors(self):
+        self.assertIsBadFormula("t1 {\ninit:\n2 + 3 +\n}\n",
+                                "unexpected newline",3)
+        self.assertIsBadFormula("t1 {\ninit:\n3 4\n}\n",
+                                "unexpected number '4.0'",3)
+        
+    def assertIsBadFormula(self,s,message,line):
+        t1 = self.parse(s)
+        self.failUnless(absyn.CheckTree(t1), "parse error not detected")
+        formula = t1.children[0]
+        self.failUnless(formula.type == "formula")
+        err = formula.children[0]
+        self.failUnless(err.type == "error")
+        #print err.leaf
+        self.assertNotEqual(re.search(message,err.leaf),None,
+                            ("bad error message text '%s'", err.leaf))
+        self.assertNotEqual(re.search(("line %s" % line),err.leaf),None,
+                            ("bad error mesage line number in '%s'", err.leaf)) 
+        
     def assertIsValidParse(self,t1):
         self.failUnless(absyn.CheckTree(t1))
         errors = self.allNodesOfType(t1,"error")
         self.assertEqual(errors,[])
         
     def assertParsesEqual(self, s1, s2):
-        t1 = self.parser.parse(self.makeMinimalFormula(s1))
-        t2 = self.parser.parse(self.makeMinimalFormula(s2))
+        t1 = self.parse(self.makeMinimalFormula(s1))
+        t2 = self.parse(self.makeMinimalFormula(s2))
         self.assertTreesEqual(t1,t2)
         
     def assertTreesEqual(self, t1, t2):
