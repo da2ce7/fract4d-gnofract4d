c9107fe38f218c7bb53e1ad8da6c0c079f5cc325
Edwin Young <edwin@bathysphere.org>
2003-07-22 05:14:08 +0000

    inconclusive fiddling towards IR tree

--- libfract4d/compiler/ir.py
+++ libfract4d/compiler/ir.py
@@ -1,6 +1,8 @@
 # Intermediate representation. The Translate module converts an Absyn tree
 # into an IR tree.
 
+# this representation copied largely from Appel Chapter 7
+
 import types
 import string
 import fracttypes
@@ -14,5 +16,24 @@ class Exp(T):
     def __init__(self, datatype):
         T.__init__(self,datatype)
 
+class Const(Exp):
+    def __init__(self, value, datatype):
+        Exp.__init__(self, datatype)
+        self.value = value
+
+class Name(Exp):
+    def __init__(self, label, datatype):
+        Exp.__init__(self, datatype)
+        self.label = label
+    
+class Temp(Exp):
+    def __init__(self, temp, datatype):
+        Exp.__init__(self, datatype)
+        self.temp = temp
+    
+class Binop(Exp):
+    def __init__(self, op, left, right, datatype):
+        Exp.__init__(self, datatype)
+        (self.op, self.left, self.right) = (op,left,right)
 
 
