c631445ea5b32e31f602538d4b79bcca304ae7ce
Edwin Young <edwin@bathysphere.org>
2003-11-09 17:41:39 +0000

    some pattern-matching goop

--- libfract4d/compiler/codegen.py
+++ libfract4d/compiler/codegen.py
@@ -4,5 +4,35 @@
 
 import ir
 import symbol
+import re
+import types
 
+# implement naive tree matching. We match an ir tree against a nested list of classes
+def match(tree, template):
+    if tree == None:
+        return template == None
 
+    if template == None:
+        return 0
+
+    if isinstance(template,types.ListType):
+        object = template[0]
+        children = template[1:]
+    else:
+        object = template
+        children = []
+        
+    if isinstance(tree, object):
+        if children != []:
+            if tree.children == None: return 0 
+            for (child, matchChild) in zip(tree.children,children):
+                if not match(child,matchChild):
+                    return 0
+        return 1
+    else:
+        return 0
+    
+template = "[Binop, Const, Const]"
+
+def expand(template):
+    return re.sub(r'(\w+)',r'ir.\1',template)
