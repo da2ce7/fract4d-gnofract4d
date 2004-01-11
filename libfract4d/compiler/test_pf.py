9b600b6a3d4ae5086f2bf8786d01e5e09e4377c9
Edwin Young <edwin@bathysphere.org>
2004-01-11 16:53:54 +0000

    can call calc from python

--- libfract4d/compiler/test_pf.py
+++ libfract4d/compiler/test_pf.py
@@ -15,12 +15,25 @@ class PfTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testLoad(self):
+    def testBasic(self):
         handle = pf.load("./test-out.so")
         pfunc = pf.create(handle)
+
+        # 1 param
         pf.init(pfunc,0.001,[0.5])
+        # empty param array
+        pf.init(pfunc,0.001,[])
+
+        # a point which doesn't bail out
+        result = pf.calc(pfunc,[1.5,0.0,0.0,0.0],100,100,0,0,0)
+        self.assertEqual(result,(100,1,0.0)) 
+        # one which does
+        result = pf.calc(pfunc,[17.5,14.0,0.0,0.0],100,100,0,0,0)
+        self.assertEqual(result,(1,0,0.0)) 
+        
         pfunc = None
         handle = None
+
 
     def testBadLoad(self):
         # wrong arg type/number
@@ -32,6 +45,15 @@ class PfTest(unittest.TestCase):
 
         # not a DLL
         self.assertRaises(ValueError,pf.load,"test_pf.py")
+
+    def testBadInit(self):
+        handle = pf.load("./test-out.so")
+        pfunc = pf.create(handle)
+        self.assertRaises(TypeError,pf.init,pfunc,0.001,72)
+        self.assertRaises(ValueError,pf.init,7,0.00,[0.4])
+        self.assertRaises(ValueError,pf.init,pfunc,0.001,[0.0]*21)
+        pfunc = None
+        handle = None
         
 def suite():
     return unittest.makeSuite(PfTest,'test')
