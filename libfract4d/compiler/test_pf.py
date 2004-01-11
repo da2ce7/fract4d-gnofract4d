ca3a4531f367bb1174c7191cb5a36e9f2ee6fcd3
Edwin Young <edwin@bathysphere.org>
2004-01-11 18:11:15 +0000

    added text-mode render test, mbrot function

--- libfract4d/compiler/test_pf.py
+++ libfract4d/compiler/test_pf.py
@@ -3,20 +3,26 @@
 import unittest
 import string
 import sys
-
+import fc
+import os.path
 
 sys.path.append("build/lib.linux-i686-2.2") # FIXME
 import pf
 
 class PfTest(unittest.TestCase):
     def setUp(self):
-        pass
-    
+        if os.path.exists("./test-pf.so"):
+            return
+        compiler = fc.Compiler()
+        compiler.load_formula_file("./gf4d.frm")
+        f = compiler.get_formula("gf4d.frm","Mandelbrot")
+        compiler.generate_code(f,"test-pf.so")
+        
     def tearDown(self):
         pass
 
     def testBasic(self):
-        handle = pf.load("./test-out.so")
+        handle = pf.load("./test-pf.so")
         pfunc = pf.create(handle)
 
         # 1 param
@@ -25,7 +31,7 @@ class PfTest(unittest.TestCase):
         pf.init(pfunc,0.001,[])
 
         # a point which doesn't bail out
-        result = pf.calc(pfunc,[1.5,0.0,0.0,0.0],100,100,0,0,0)
+        result = pf.calc(pfunc,[0.15,0.0,0.0,0.0],100,100,0,0,0)
         self.assertEqual(result,(100,1,0.0)) 
         # one which does
         result = pf.calc(pfunc,[17.5,14.0,0.0,0.0],100,100,0,0,0)
@@ -38,7 +44,25 @@ class PfTest(unittest.TestCase):
         pfunc = None
         handle = None
 
-
+    def testMiniTextRender(self):
+        handle = pf.load("./test-pf.so")
+        pfunc = pf.create(handle)
+        pf.init(pfunc,0.001,[])
+        image = []
+        for y in xrange(-20,20):
+            line = []
+            for x in xrange(-20,20):
+                (iter,fate,dist) = pf.calc(pfunc,[x/10.0,y/10.0,0,0],100,100)
+                if(fate == 1):
+                    line.append("#")
+                else:
+                    line.append(" ")
+            image.append(string.join(line,""))
+        printable_image = string.join(image,"\n")
+        self.assertEqual(printable_image[0], " ")
+        self.assertEqual(printable_image[20*41+20],"#") # in the middle
+        # print printable_image # shows low-res mbrot in text mode 
+        
     def testBadLoad(self):
         # wrong arg type/number
         self.assertRaises(TypeError,pf.load,1)
@@ -51,7 +75,7 @@ class PfTest(unittest.TestCase):
         self.assertRaises(ValueError,pf.load,"test_pf.py")
 
     def testBadInit(self):
-        handle = pf.load("./test-out.so")
+        handle = pf.load("./test-pf.so")
         pfunc = pf.create(handle)
         self.assertRaises(TypeError,pf.init,pfunc,0.001,72)
         self.assertRaises(ValueError,pf.init,7,0.00,[0.4])
@@ -60,7 +84,7 @@ class PfTest(unittest.TestCase):
         handle = None
 
     def testBadCalc(self):
-        handle = pf.load("./test-out.so")
+        handle = pf.load("./test-pf.so")
         pfunc = pf.create(handle)
         pf.init(pfunc,0.001,[])
         self.assertRaises(ValueError,pf.calc,0,[1.0,2.0,3.0,4.0],100,100)
@@ -68,7 +92,7 @@ class PfTest(unittest.TestCase):
         pfunc = None
 
     def testShutdownOrder(self):
-        handle = pf.load("./test-out.so")
+        handle = pf.load("./test-pf.so")
         pfunc = pf.create(handle)
         pfunc2 = pf.create(handle)
         handle = None
