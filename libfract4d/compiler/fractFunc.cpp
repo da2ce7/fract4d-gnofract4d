2dca5a46add805fe2b2afeda11806aa11fe5a07b
Edwin Young <edwin@bathysphere.org>
2004-01-23 05:21:16 +0000

    C extension builds. Not yet Python-callable

---
+++ libfract4d/compiler/fractFunc.cpp
@@ -0,0 +1,304 @@
+#include "fractFunc.h"
+#include <stdio.h>
+
+
+void
+fractFunc::update_matrix(double *params)
+{
+    d one = d(1.0);
+    d zero = d(0.0);
+    dmat4 id = identity3D<d>(params[MAGNITUDE],zero);
+
+    rot = id * 
+        rotXY<d>(params[XYANGLE],one,zero) *
+        rotXZ<d>(params[XZANGLE],one,zero) * 
+        rotXW<d>(params[XWANGLE],one,zero) *
+        rotYZ<d>(params[YZANGLE],one,zero) *
+        rotYW<d>(params[YWANGLE],one,zero) *
+        rotZW<d>(params[ZWANGLE],one,zero);
+}
+
+fractFunc::fractFunc(
+        d *params_,
+	int eaa_,
+	int maxiter_,
+	int nThreads_,
+	bool auto_deepen_,
+	IFractWorker *fw,
+	IImage *im_, 
+	IFractalSite *site_)
+{
+    site = site_;
+    im = im_;
+    ok = true;
+    worker = fw;
+    params = params_;
+
+    eaa = eaa_;
+    depth = eaa == AA_NONE ? 1 : 2;
+    maxiter = maxiter_;
+    nThreads = nThreads_;
+    auto_deepen = auto_deepen_;
+
+    update_matrix(params);
+    rot = rot/im->Xres();
+    deltax = rot[VX];
+    deltay = rot[VY];
+    ddepth = (depth*2);
+    delta_aa_x = deltax / ddepth;    
+    delta_aa_y = deltay / ddepth;
+    
+    topleft = vec4<d>(params[XCENTER],params[YCENTER],
+		      params[ZCENTER],params[WCENTER]) -
+        deltax * im->Xres() / 2.0 -
+        deltay * im->Yres() / 2.0;
+
+    d depthby2 = ddepth/2.0;
+    aa_topleft = topleft - (delta_aa_y + delta_aa_x) * depthby2;
+    
+    nTotalHalfIters = nTotalDoubleIters = nTotalK = 0;
+    clear();
+
+    status_changed(GF4D_FRACTAL_COMPILING);
+
+    worker->set_fractFunc(this);
+
+    last_update_y = 0;
+};
+
+fractFunc::~fractFunc()
+{
+
+}
+
+void 
+fractFunc::clear()
+{
+    im->clear();    
+}
+
+bool
+fractFunc::update_image(int i)
+{
+    image_changed(0,last_update_y,im->Xres(),i);
+    progress_changed((float)i/(float)im->Yres());
+    last_update_y = i;
+    return try_finished_cond();
+}
+
+// see if the image needs more (or less) iterations to display
+// properly returns +ve if more are required, -ve if less are
+// required, 0 if it's correct. This is a very poor heuristic - a
+// histogram approach would be better
+
+int
+fractFunc::updateiters()
+{
+    // add up all the subtotals
+    worker->stats(&nTotalDoubleIters,&nTotalHalfIters,&nTotalK);
+
+    double doublepercent = ((double)nTotalDoubleIters*AUTO_DEEPEN_FREQUENCY*100)/nTotalK;
+    double halfpercent = ((double)nTotalHalfIters*AUTO_DEEPEN_FREQUENCY*100)/nTotalK;
+		
+    if(doublepercent > 1.0) 
+    {
+        // more than 1% of pixels are the wrong colour! 
+        // quelle horreur!
+        return 1;
+    }
+
+    if(doublepercent == 0.0 && halfpercent < 0.5 && 
+       maxiter > 32)
+    {
+        // less than .5% would be wrong if we used half as many iters
+        // therefore we are working too hard!
+        return -1;
+    }
+    return 0;
+}
+
+void fractFunc::draw_aa()
+{
+    int w = im->Xres();
+    int h = im->Yres();
+
+    reset_counts();
+
+    reset_progress(0.0);
+
+    // if we have multiple threads,make sure they don't modify
+    // pixels the other thread will look at - that wouldn't be 
+    // an error per se but would make drawing nondeterministic,
+    // which I'm trying to avoid
+    for(int i = 0; i < nThreads; ++i)
+    {
+        last_update_y = 0;
+        for(int y = i; y < h ; y+= nThreads) {
+	    worker->row_aa(0,y,w);
+	    if(update_image(y))
+	    {
+		break;
+	    }
+        }
+        reset_progress(1.0);
+    }
+
+}
+
+void fractFunc::reset_counts()
+{
+    worker->reset_counts();
+    
+    nTotalHalfIters = nTotalDoubleIters = nTotalK = 0;
+}
+
+void fractFunc::reset_progress(float progress)
+{
+    worker->flush();
+    image_changed(0,0,im->Xres(),im->Yres());
+    progress_changed(progress);
+}
+
+void fractFunc::draw_all()
+{
+    status_changed(GF4D_FRACTAL_CALCULATING);
+    
+    draw(8,8);
+    
+    int deepen;
+    while((deepen = updateiters()) > 0)
+    {
+        maxiter *= 2;
+        status_changed(GF4D_FRACTAL_DEEPENING);
+        draw(8,1);
+    }
+    
+    if(eaa > AA_NONE) {
+        status_changed(GF4D_FRACTAL_ANTIALIASING);
+        draw_aa();
+    }
+    
+    // we do this after antialiasing because otherwise sometimes the
+    // aa pass makes the image shallower, which is distracting
+    if(deepen < 0)
+    {
+        maxiter /= 2;
+    }
+    status_changed(GF4D_FRACTAL_DONE);
+    progress_changed(0.0);
+}
+
+void fractFunc::draw(int rsize, int drawsize)
+{
+    reset_counts();
+
+    if(nThreads > 1)
+    {
+        draw_threads(rsize, drawsize);
+        return;
+    }
+
+    int x,y;
+    int w = im->Xres();
+    int h = im->Yres();
+
+    /* reset progress indicator & clear screen */
+    last_update_y = 0;
+    reset_progress(0.0);
+
+    // first pass - big blocks and edges
+    for (y = 0 ; y < h - rsize ; y += rsize) 
+    {
+        // main large blocks 
+        for ( x = 0 ; x< w - rsize ; x += rsize) 
+        {
+            worker->pixel ( x, y, drawsize, drawsize);
+        }
+        // extra pixels at end of lines
+        for(int y2 = y; y2 < y + rsize; ++y2)
+        {
+            worker->row (x, y2, w-x);
+        }
+        if(update_image(y)) 
+        {
+            goto done;
+        }
+    }
+    // remaining lines
+    for ( ; y < h ; y++)
+    {
+        worker->row(0,y,w);
+        if(update_image(y)) 
+        {
+            goto done;
+        }
+    }
+
+    last_update_y = 0;
+    reset_progress(0.0);
+
+    // fill in gaps in the rsize-blocks
+    for ( y = 0; y < h - rsize; y += rsize) {
+        for(x = 0; x < w - rsize ; x += rsize) {
+            worker->box(x,y,rsize);
+        }
+        if(update_image(y))
+        {
+            goto done;
+        }
+    }
+
+ done:
+    /* refresh entire image & reset progress bar */
+    reset_progress(1.0);
+}
+
+void fractFunc::draw_threads(int rsize, int drawsize)
+{
+    int x,y;
+    int w = im->Xres();
+    int h = im->Yres();
+
+    last_update_y = 0;
+    reset_progress(0.0);
+
+    // first pass - big blocks and edges
+    // do this in current thread - it's fast anyway
+    for (y = 0 ; y < h - rsize ; y += rsize) 
+    {
+        // main large blocks 
+        for ( x = 0 ; x< w - rsize ; x += rsize) 
+        {
+            worker->pixel ( x, y, drawsize, drawsize);
+        }
+        // extra pixels at end of lines
+        for(int y2 = y; y2 < y + rsize; ++y2)
+        {
+            worker->row (x, y2, w-x);
+        }
+        if(update_image(y))
+        {
+            goto done;
+        }
+    }
+
+    // remaining lines
+    for ( y = h > rsize ? h - rsize : 0 ; y < h ; y++)
+    {
+        worker->row(0,y,w);
+        if(update_image(y)) goto done;
+    }
+
+    reset_progress(0.0);
+
+    last_update_y = 0;
+    // fill in gaps in the rsize-blocks
+    for ( y = 0; y < h - rsize; y += rsize) {
+        worker->box_row(w,y,rsize);
+        if(update_image(y)) goto done;
+    }
+    
+ done:
+    reset_progress(1.0);
+}
+
---
+++ libfract4d/compiler/fractFunc.h
@@ -0,0 +1,151 @@
+/* Gnofract4D -- a little fractal generator-browser program
+ * Copyright (C) 1999-2002 Edwin Young
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef _FRACTFUNC_H_
+#define _FRACTFUNC_H_
+
+#include "image_public.h"
+#include "pointFunc_public.h"
+#include "fractWorker_public.h"
+#include "vectors.h"
+#include "fract_public.h"
+
+typedef double d;
+typedef vec4<d> dvec4;
+typedef mat4<d> dmat4;
+
+
+
+/* this contains stuff which is useful for drawing the fractal,
+   but can be recalculated at will, so isn't part of the fractal's
+   persistent state. We create a new one each time we start drawing. This one
+   parcels up the work which is actually performed by the fractThreadFuncs
+ */
+
+class fractFunc {
+ public:
+    fractFunc(
+	d *params,
+	int eaa,
+	int maxiter,
+	int nThreads_,
+	bool auto_deepen,
+	IFractWorker *fw,
+	IImage *_im, 
+	IFractalSite *_site);
+    ~fractFunc();
+   
+    void update_matrix(double *params);
+
+    void draw_all();
+    void draw(int rsize, int drawsize);    
+    void draw_aa();
+    int updateiters();
+
+    friend class STFractWorker;
+
+    // callback wrappers
+    inline void parameters_changed()
+	{
+	    site->parameters_changed();
+	}
+    inline void image_changed(int x1, int x2, int y1, int y2)
+	{
+	    site->image_changed(x1,x2,y1,y2);
+	}
+    inline void progress_changed(float progress)
+	{
+	    site->progress_changed(progress);
+	}
+    inline void status_changed(int status_val)
+	{
+	    site->status_changed(status_val);
+	}
+    inline bool try_finished_cond()
+	{
+	    return site->is_interrupted();
+	}
+
+ private:
+    // MEMBER VARS
+
+    bool ok; // did this instance get constructed ok?
+    // (* this should really be done with exns but they are unreliable 
+    //  * in the presence of pthreads - grrr *)
+
+    // do every nth pixel twice as deep as the others to
+    // see if we need to auto-deepen
+    enum { AUTO_DEEPEN_FREQUENCY = 30 };
+
+    // for callbacks
+    IFractalSite *site;
+
+    dmat4 rot; // scaled rotation matrix
+    dvec4 deltax, deltay; // step from 1 pixel to the next in x,y directions
+    dvec4 delta_aa_x, delta_aa_y; // offset between subpixels
+    dvec4 topleft; // top left corner of screen
+    dvec4 aa_topleft; // topleft - offset to 1st subpixel to draw
+
+    int depth;    // antialias depth
+    int eaa;
+    int maxiter;
+    int nThreads;
+    bool auto_deepen;
+
+    d *params;
+
+    d ddepth;     // double version of antialias depth
+
+    // n pixels correctly classified that would be wrong 
+    // if we halved iterations
+    int nTotalHalfIters;
+    // n pixels misclassified that would be correct 
+    // if we doubled the iterations
+    int nTotalDoubleIters; 
+    int nTotalK;	// number of pixels calculated    
+
+    // last time we redrew the image to this line
+    int last_update_y; 
+
+
+    IImage *im;    // pointer to image passed in to ctor
+    pointFunc *pf; // function for calculating 1 point
+
+    IFractWorker *worker;
+
+    void send_quit();
+
+    // redraw the image to this line
+    // also checks for interruptions & returns true if we should stop
+    bool update_image(int i);
+
+    // clear auto-deepen and last_update
+    void reset_counts();
+    void reset_progress(float progress);
+
+
+    // calculate the whole image using worker threads
+    void draw_threads(int rsize, int drawsize);
+
+    // reset image
+    void clear();
+};
+
+#endif /* _FRACTFUNC_H_ */
