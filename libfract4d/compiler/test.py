144b209d450b3091abc53512b9c22a997fba575a
Edwin Young <edwin@bathysphere.org>
2004-01-25 17:59:40 +0000

    added initial parfile & colormap support

--- libfract4d/compiler/test.py
+++ libfract4d/compiler/test.py
@@ -9,6 +9,8 @@ import test_canon
 import test_codegen
 import test_fc
 import test_fract4d
+import test_colormap
+import test_parfile
 
 def suite():
     s1 = test_fractlexer.suite()
@@ -19,8 +21,10 @@ def suite():
     s6 = test_codegen.suite()
     s7 = test_fc.suite()
     s8 = test_fract4d.suite()
+    s9 = test_colormap.suite()
+    s10 = test_parfile.suite()
     
-    return unittest.TestSuite((s1, s2, s3, s4, s5, s6, s7, s8))
+    return unittest.TestSuite((s1, s2, s3, s4, s5, s6, s7, s8, s9, s10))
 
 def main():
     unittest.main(defaultTest='suite')
---
+++ libfract4d/compiler/test_parfile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# test cases for parfile.py
+
+import parfile
+
+import string
+import unittest
+
+class ParTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testColors(self):
+        self.assertEqual(parfile.colorRange(""), [])
+
+        colors = parfile.colorRange("05F")
+        self.assertEqual(len(colors),1)
+        
+        colors = parfile.colorRange("05F<63>DISDISDIS<89>uxxuxxvyyvyywzz<94>05F")
+        self.assertEqual(len(colors),256)
+
+        self.assertRaises(RuntimeError,parfile.colorRange, "&*(")
+        self.assertRaises(RuntimeError,parfile.colorRange, "00")
+        self.assertRaises(RuntimeError,parfile.colorRange, "000<0>000")
+        self.assertRaises(RuntimeError,parfile.colorRange, "<1>000")
+        self.assertRaises(RuntimeError,parfile.colorRange, "<nootherangle")
+        self.assertRaises(ValueError,parfile.colorRange, "<>")
+def suite():
+    return unittest.makeSuite(ParTest,'test')
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
