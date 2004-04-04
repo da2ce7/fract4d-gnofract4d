9f7858c22e09c10d3a9f3c48f78b87f3d7408429
Edwin Young <edwin@bathysphere.org>
2004-04-04 19:16:39 +0000

    extra undo code

--- gui/test_undo.py
+++ gui/test_undo.py
@@ -27,47 +27,137 @@ class UndoTest(unittest.TestCase):
         if status == 0:
             gtk.main_quit()
 
-    def testCreate(self):
-        undoer = undo.Sequence()
-        self.assertEqual(len(undoer.history),0)
-        self.assertEqual(undoer.can_undo(),False)
-        self.assertEqual(undoer.can_redo(),False)
-        self.assertEqual(undoer.pos,0)
+    def assertUndoStatus(self,undoer,should_undo,should_redo):
+        self.assertEqual(undoer.can_undo(),should_undo)
+        self.assertEqual(undoer.can_redo(),should_redo)
+        self.assertExternalAndInternalStatusMatch(undoer)
+
+    def assertExternalAndInternalStatusMatch(self,undoer):
+        self.assertEqual(self.undo_cb_status.count, undoer.can_undo())
+        self.assertEqual(self.redo_cb_status.count, undoer.can_redo())
         
+    def testCreateUndoRedo(self):        
         status = Status()
+        self.undo_cb_status = Status()
+        self.redo_cb_status = Status()
+        
         def inc_status():
             status.count += 1
             
         def dec_status():
             status.count -= 1
 
-        self.assertEqual(status.count,0)
+        def set_undoable(sequence,state):
+            #print "undo_status: %s" % state
+            self.undo_cb_status.count = state
 
+        def set_redoable(sequence,state):
+            #print "redo_status: %s" % state
+            self.redo_cb_status.count = state
+            
+        self.assertEqual(status.count,0)
+        self.assertEqual(self.undo_cb_status.count,0)
+        self.assertEqual(self.redo_cb_status.count,0)
+                
+        # create sequence
+        undoer = undo.Sequence()
+        undoer.connect('can-undo',set_undoable)
+        undoer.connect('can-redo',set_redoable)
+        
+        self.assertEqual(undoer.can_undo(),False)
+        self.assertEqual(undoer.can_redo(),False)
+        self.assertExternalAndInternalStatusMatch(undoer)
+        
+        self.assertEqual(undoer.pos,0)
+        self.assertEqual(len(undoer.history),0)
+        
         # perform an action
-        inc_status()
         undoer.do(inc_status,dec_status)
         
         self.assertEqual(status.count,1)
+        self.assertUndoStatus(undoer,True,False)        
         self.assertEqual(len(undoer.history),1)
-        self.assertEqual(undoer.can_undo(),True)
-        self.assertEqual(undoer.can_redo(),False)
         self.assertEqual(undoer.pos,1)
-        
+
         # undo it
         undoer.undo()
+
+        # check externals
         self.assertEqual(status.count,0)
+        self.assertUndoStatus(undoer,False,True)
+        
+        # check internals
         self.assertEqual(len(undoer.history),1)
-        self.assertEqual(undoer.can_undo(),False)
-        self.assertEqual(undoer.can_redo(),True)
         self.assertEqual(undoer.pos,0)
         
         # redo it
         undoer.redo()
+
+        # check externals
         self.assertEqual(status.count,1)
+        self.assertUndoStatus(undoer,True,False)
+        
+        # check internals
         self.assertEqual(len(undoer.history),1)
-        self.assertEqual(undoer.can_undo(),True)
-        self.assertEqual(undoer.can_redo(),False)
         self.assertEqual(undoer.pos,1)
+
+    def testUndoAndRedoAreAssociated(self):
+        status = Status()
+
+        def inc_status():
+            status.count += 1
+            
+        def dec_status():
+            status.count -= 1
+
+        def make_status_string():
+            status.count = "foo"
+
+        def make_status_int():
+            status.count = 1
+                    
+        self.assertEqual(status.count,0)
+
+        # create sequence
+        undoer = undo.Sequence()
+        undoer.do(inc_status,dec_status)
+        undoer.do(make_status_string, make_status_int)
+
+        self.assertEqual(status.count,"foo")
+        undoer.undo()
+        self.assertEqual(status.count,1)
+        undoer.undo()
+        self.assertEqual(status.count,0)
+
+    def testDoRemovesRedoStack(self):
+        status = Status()
+
+        def inc_status():
+            status.count += 1
+            
+        def dec_status():
+            status.count -= 1
+                    
+        self.assertEqual(status.count,0)
+
+        # perform 3 actions, undo 2, do 1 again
+        undoer = undo.Sequence()
+        undoer.do(inc_status,dec_status)
+        undoer.do(inc_status,dec_status)
+        undoer.do(inc_status,dec_status)
+        undoer.undo()
+        undoer.undo()
+        self.assertEqual(status.count,1)
+        
+        undoer.do(inc_status,dec_status)
+        self.assertEqual(len(undoer.history),2)
+        self.assertEqual(undoer.can_redo(), False)
+        self.assertEqual(status.count,2)
+
+    def testInvalidOperationsThrow(self):
+        undoer = undo.Sequence()
+        self.assertRaises(ValueError,undoer.undo)
+        self.assertRaises(ValueError,undoer.redo)
         
 def suite():
     return unittest.makeSuite(UndoTest,'test')
