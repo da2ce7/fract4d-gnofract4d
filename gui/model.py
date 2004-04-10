d091744d5fecfa22837e40cf39d4adb8adfa4b94
Edwin Young <edwin@bathysphere.org>
2004-04-10 23:25:35 +0000

    undo/redo for positions

--- gui/model.py
+++ gui/model.py
@@ -8,30 +8,50 @@ class Model:
         # undo history
         self.seq = undo.Sequence()
 
-        # used to prevent recursive sets of commands happening
-        # at the same time
-        self.command_in_progess = False
+        # used to prevent undo/redo from triggering new commands
+        self.callback_in_progress = False
         self.f = f
         self.old_f = self.f.copy_f()
         self.f.connect('parameters-changed',self.onParametersChanged)
-        
+
+    def block_callbacks(self):
+        self.callback_in_progress = True
+
+    def unblock_callbacks(self):
+        self.callback_in_progress = False
+
+    def callbacks_allowed(self):
+        return not self.callback_in_progress
+    
     def onParametersChanged(self,*args):
+        if not self.callbacks_allowed():
+            return
+        
         current = self.f.copy_f()
         def redo():
             self.f.freeze()
             self.f.set_fractal(current)
-            self.f.thaw()
-            
-        previous = self.old_f
+            if self.f.thaw():
+                self.block_callbacks()
+                self.f.changed()
+                self.f.unblock_callbacks()
+                
+        previous = self.old_f        
         def undo():
             self.f.freeze()
             self.f.set_fractal(previous)
-            self.f.thaw()
-            
-        self.seq.do(redo,undo)
+            if self.f.thaw():
+                self.block_callbacks()
+                self.f.changed()
+                self.unblock_callbacks()
 
+        self.seq.do(redo,undo)
+        self.old_f = current
+        
     def undo(self):
-        self.seq.undo()
+        if self.seq.can_undo():
+            self.seq.undo()
 
     def redo(self):
-        self.seq.redo()
+        if self.seq.can_redo():
+            self.seq.redo()
