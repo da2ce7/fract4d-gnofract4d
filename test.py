66feae120e2d81052eeb46102797aaf34cab8bf4
Edwin Young <edwin@bathysphere.org>
2004-05-02 05:29:09 +0000

    added toplevel test script

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# run all the tests
+import os
+import sys
+import unittest
+
+print "Running all unit tests. This may take several minutes."
+
+def main():
+    os.chdir('fract4d')
+    os.system('./test.py')
+    os.chdir('../fract4dgui')
+    os.system('./test.py')
+    
+if __name__ == '__main__':
+    main()
+
+
+
+
