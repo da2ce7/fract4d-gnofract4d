d5c58af80ff39df7aab4ffaf8e62296cbf7ab816
Edwin Young <edwin@bathysphere.org>
2004-05-30 01:51:18 +0000

    created angle widget

--- fract4dgui/test.py
+++ fract4dgui/test.py
@@ -5,14 +5,18 @@ import test_gtkfractal
 import test_undo
 import test_model
 import test_preferences
+import test_fourway
+import test_angle
 
 def suite():
     s1 = test_gtkfractal.suite()
     s2 = test_undo.suite()
     s3 = test_model.suite()
     s4 = test_preferences.suite()
+    s5 = test_fourway.suite()
+    s6 = test_angle.suite()
     
-    return unittest.TestSuite((s1,s2,s3, s4))
+    return unittest.TestSuite((s1,s2,s3,s4,s5,s6))
 
 def main():
     unittest.main(defaultTest='suite')
