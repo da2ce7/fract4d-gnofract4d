c204c4ebbb5300a98e3977d414f32295e7dac122
Edwin Young <edwin@bathysphere.org>
2005-03-04 06:45:00 +0000

    more preprocessor stuff

--- fract4d/preprocessor.py
+++ fract4d/preprocessor.py
@@ -4,37 +4,64 @@ ifdef_re = re.compile(r'\s*\$ifdef(\s+(?P<var>[a-z][a-z0-9_]*))?',
                       re.IGNORECASE)
 endif_re = re.compile(r'\s*\$endif', re.IGNORECASE)
 
+define_re = re.compile(r'\s*\$define(\s+(?P<var>[a-z][a-z0-9_]*))?',
+                       re.IGNORECASE)
+
 class Error(Exception):
     def __init__(self,msg):
         Exception.__init__(self,msg)
         pass
-    
+
+class StackEntry:
+    def __init__(self, line_num, isTrue):
+        self.line_num = line_num
+        self.isTrue = isTrue
 class T:
+
+    def currently_true(self):
+        return self.ifdef_stack == [] or self.ifdef_stack[-1].isTrue
+
+    def get_var(self, m, i, type):
+        var = m.group("var")
+        if not var:
+            raise Error("%d: %s without variable" % (i, type))
+        return var
+    
     def __init__(self, s):
+        self.vars = {}
         lines = s.splitlines(True)
-        ifdef_stack = []
+        self.ifdef_stack = []
         out_lines = []
         i = 1
         for line in lines:
             m = ifdef_re.match(line)
             if m:
-                var = m.group("var")
-                if var:
-                    ifdef_stack.append(i)
-                else:
-                    raise Error("%d: $IFDEF without variable" % i)
+                var = self.get_var(m,i, "$IFDEF")
+                isTrue = False
+                if self.vars.has_key(var):
+                    isTrue = self.currently_true()
+                        
+                self.ifdef_stack.append(StackEntry(i, isTrue))
             elif endif_re.match(line):
-                if ifdef_stack == []:
+                if self.ifdef_stack == []:
                     raise Error("%d: $ENDIF without $IFDEF" % i)
-                ifdef_stack.pop()
+                self.ifdef_stack.pop()
             else:
-                if ifdef_stack == []:
-                    out_lines.append(line)
+                m = define_re.match(line)
+                if m:
+                    # a $define
+                    var = self.get_var(m,i, "$DEFINE")
+                    self.vars[var] = 1
+                else:
+                    # just a line
+                    if self.currently_true():
+                        out_lines.append(line)
 
             i += 1
             
-        if ifdef_stack != []:
-            raise Error("%d: $IFDEF without $ENDIF" % ifdef_stack[-1])
+        if self.ifdef_stack != []:
+            raise Error("%d: $IFDEF without $ENDIF" % \
+                        self.ifdef_stack[-1].line_num)
         
         self._out = "".join(out_lines)
         
