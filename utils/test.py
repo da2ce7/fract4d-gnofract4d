577937062a564d029db21181cd01231c07a9dcb7
Edwin Young <edwin@bathysphere.org>
2005-09-25 17:06:51 +0000

    initial check-in of flickr integration utilities

--- utils/test.py
+++ utils/test.py
@@ -3,10 +3,12 @@
 import unittest
 
 import test_makemap
+import test_flickr
 
 def suite():
     tests = (
         test_makemap.suite(),
+        test_flickr.suite()
         )
     return unittest.TestSuite(tests)
 
