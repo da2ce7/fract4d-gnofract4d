8609da6bd643b5ffcf5371d533fdfa9813f11d1b
Edwin Young <edwin@bathysphere.org>
2005-11-20 17:07:53 +0000

    added files to repository

---
+++ createdocs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# run this to update some tables in the documentation
+
+# making this a separate cmd (not part of setup.py) because importing
+# gtk was causing "setup.py build" to crash - inexplicable...
+
+# also the other python versions don't have gtk as a module,
+# so were reporting errors.
+
+def create_stdlib_docs():
+    'Autogenerate docs'
+    try:
+        # create list of stdlib functions
+        import fract4d.createdocs
+        fract4d.createdocs.main("doc/gnofract4d-manual/C/stdlib.xml")
+
+        # create list of mouse and GUI commands
+        import fract4dgui.createdocs
+        fract4dgui.createdocs.main("doc/gnofract4d-manual/C/commands.xml")
+
+        
+    except Exception, err:
+        print >>sys.stderr,\
+              "Problem creating docs. Online help will be incomplete."
+        print >>sys.stderr, err
+
+create_stdlib_docs()
