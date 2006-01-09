96960811801fdee2544afa3ab27e8505f38dbf8a
Edwin Young <edwin@bathysphere.org>
2006-01-09 02:36:04 +0000

    added fract_stdlib files (not used yet)

---
+++ fract4d/c/fract_stdlib.cpp
@@ -0,0 +1,4 @@
+// These functions are implementations of the more complicated
+// complex-number functions. They're implemented here rather than
+// in stdlib.py to speed up compilation of complex formulas.
+
