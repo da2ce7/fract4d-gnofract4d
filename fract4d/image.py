17ade34ca8db5409e9549bd184a89daa00424dea
Edwin Young <edwin@bathysphere.org>
2006-03-01 21:46:10 +0000

    further tinkering with image.py and test_image.py

--- fract4d/image.py
+++ fract4d/image.py
@@ -11,16 +11,34 @@ class T:
     UNKNOWN=255
     BLACK=[0,0,0]
     WHITE=[255,255,255]
-    def __init__(self,xsize,ysize,img):
+    def __init__(self,xsize,ysize):
         self.xsize = xsize
         self.ysize = ysize
-        self.fate_buf = fract4dc.image_fate_buffer(img,0,0)
-        self.image_buf = fract4dc.image_buffer(img,0,0)
-        self.img = img
+        self._img = fract4dc.image_create(xsize,ysize)
+        self.update_bufs()
 
+    def update_bufs(self):
+        self.fate_buf = fract4dc.image_fate_buffer(self._img,0,0)
+        self.image_buf = fract4dc.image_buffer(self._img,0,0)
+
+    def resize(self,x,y):
+        fract4dc.image_resize(self._img, x, y)
+        self.xsize = x
+        self.ysize = y
+        self.update_bufs()
+        
+    def clear(self):
+        fract4dc.image_clear(self._img)
+        
     def pos(self,x,y,size):
         return size * (y * self.xsize + x)
-    
+
+    def fate_buffer(self,x=0,y=0):
+        return fract4dc.image_fate_buffer(self._img, x, y)
+
+    def image_buffer(self,x=0,y=0):
+        return fract4dc.image_buffer(self._img, x, y)
+        
     def get_fate(self,x,y):
         return ord(self.fate_buf[self.pos(x,y,T.FATE_SIZE)])
 
@@ -33,6 +51,6 @@ class T:
         return map(ord,list(self.image_buf[pos:pos+T.COL_SIZE]))
 
     def get_color_index(self,x,y,sub=0):
-        return fract4dc.image_get_color_index(self.img,x,y,sub)
+        return fract4dc.image_get_color_index(self._img,x,y,sub)
     
     
--- fract4d/test_image.py
+++ fract4d/test_image.py
@@ -12,82 +12,72 @@ import types
 import testbase
 
 import image
-import fract4dc
 
 class Test(testbase.TestBase):
     def testColossalImage(self):
         try:
-            image = fract4dc.image_create(400000,300000)
+            im = image.T(400000,300000)
             self.fail("Should have raised an exception")
         except MemoryError, err:
             pass
 
-        image = fract4dc.image_create(40,30)
+        im = image.T(40,30)
         try:
-            fract4dc.image_resize(image, 400000,300000)
+            im.resize(400000,300000)
             self.fail("Should have raised an exception")
         except MemoryError, err:
+            self.assertEqual(40, im.xsize)
+            self.assertEqual(30, im.ysize)
             pass
 
-    def testImage(self):
-        image = fract4dc.image_create(40,30)
-        fract4dc.image_resize(image,80,60)
-        buf = fract4dc.image_buffer(image)
-        self.assertEqual(len(buf),80*60*3)
-
-        fate_buf = fract4dc.image_fate_buffer(image)
-        self.assertEqual(len(fate_buf),80*60*4)
+    def assertImageInvariants(self, im):
+        self.assertEqual(im.xsize*im.ysize*im.FATE_SIZE, len(im.fate_buffer()))
+        self.assertEqual(im.xsize*im.ysize*im.COL_SIZE, len(im.image_buffer()))
         
-        bytes = list(buf)
-        self.assertEqual(ord(bytes[0]),0)
-        self.assertEqual(ord(bytes[1]),0)
-        self.assertEqual(ord(bytes[2]),0)
-
-        fate_bytes = list(fate_buf)
-        for fb in fate_bytes:
-            self.assertEqual(ord(fb), 255)
-            
-        self.assertRaises(ValueError,fract4dc.image_buffer, image, -1, 0)
-        self.assertRaises(ValueError,fract4dc.image_buffer, image, 80, 0)
-        self.assertRaises(ValueError,fract4dc.image_buffer, image, 41, 67)
-
-        self.assertRaises(ValueError,fract4dc.image_fate_buffer, image, -1, 0)
-        self.assertRaises(ValueError,fract4dc.image_fate_buffer, image, 80, 0)
-        self.assertRaises(ValueError,fract4dc.image_fate_buffer, image, 41, 67)
-
-        buf = fract4dc.image_buffer(image,5,10)
-        self.assertEqual(len(buf),80*60*3 - (10*80+5)*3)
+    def testResize(self):
+        im = image.T(10,20)
+        self.assertEqual(10,im.xsize)
+        self.assertEqual(20,im.ysize)
+        self.assertImageInvariants(im)
         
-        buf = fract4dc.image_fate_buffer(image,5,10)
-        self.assertEqual(len(buf),80*60*4 - (10*80+5)*4)
+        im.resize(30,17)
+        self.assertEqual(30,im.xsize)
+        self.assertEqual(17,im.ysize)
+        self.assertImageInvariants(im)
         
-    def printStuff(self):
-        print
-        for y in xrange(xsize):
-            lower = ""
-            upper = ""
-            for x in xrange(ysize):
-                fract4dc.fw_pixel(fw,x,y,1,1)
-                fract4dc.fw_pixel_aa(fw,x,y)
-                fates = iw.get_all_fates(x,y)
-                upper += "%2x%2x " % (fates[0],fates[1])
-                lower += "%2x%2x " % (fates[2],fates[3])
-            print upper
-            print lower
-        print
+    def testClear(self):
+        # check clear() works
+        (xsize,ysize) = (61,33)
+        im = image.T(xsize, ysize)
+        im.clear()
+        buf = im.image_buffer()
+        fate_buf = im.fate_buffer()
+        self.assertEqual(
+            list(fate_buf), [chr(im.UNKNOWN)] * im.FATE_SIZE * xsize * ysize)
 
-    def print_fates(self,image,x,y):
-        buf = fract4dc.image_fate_buffer(image)
-        for i in xrange(len(buf)):
-            v = ord(buf[i])
-            if v == 255:
-                print "U",
-            else:
-                print v,
-                
-            if i % (x*4) == 4*x-1:
-                print ""            
+        bytes = map(ord,list(buf))
+        self.assertEqual(bytes, [0] * xsize * ysize * im.COL_SIZE)
 
+    def testBufferBounds(self):
+        im = image.T(40,30)
+        im.resize(80,60)
+        buf = im.image_buffer()
+        fate_buf = im.fate_buffer()
+
+        self.assertRaises(ValueError,im.image_buffer, -1, 0)
+        self.assertRaises(ValueError,im.image_buffer, 80, 0)
+        self.assertRaises(ValueError,im.image_buffer, 41, 67)
+
+        self.assertRaises(ValueError,im.fate_buffer, -1, 0)
+        self.assertRaises(ValueError,im.fate_buffer, 80, 0)
+        self.assertRaises(ValueError,im.fate_buffer, 41, 67)
+
+        buf = im.image_buffer(5,10)
+        self.assertEqual(len(buf),80*60*im.COL_SIZE - (10*80+5)*im.COL_SIZE)
+        
+        buf = im.fate_buffer(5,10)
+        self.assertEqual(len(buf),80*60*im.FATE_SIZE - (10*80+5)*im.FATE_SIZE)
+        
 def suite():
     return unittest.makeSuite(Test,'test')
 
