a22cdda185a89d81757383ba27d20fc0459a0714
Edwin Young <edwin@bathysphere.org>
2006-03-03 03:45:28 +0000

    first basically-working version of renderqueue component

--- fract4dgui/renderqueue.py
+++ fract4dgui/renderqueue.py
@@ -1,9 +1,61 @@
 # A module which manages a queue of images to render in the background
 # and a UI for the same
 
+
+import copy
+
+import gobject
+
+import gtkfractal
+
+class QueueEntry:
+    def __init__(self, f, name, w, h):
+        self.f = f
+        self.name = name
+        self.w = w
+        self.h = h
+        
 # the underlying queue object
-class T:
-    def __init__(self):
-        pass
-    
-    
+class T(gobject.GObject):
+    __gsignals__ = {
+        'done' : (
+        (gobject.SIGNAL_RUN_FIRST | gobject.SIGNAL_NO_RECURSE),
+        gobject.TYPE_NONE, ()),
+        'image-complete' : (
+        (gobject.SIGNAL_RUN_FIRST | gobject.SIGNAL_NO_RECURSE),
+        gobject.TYPE_NONE, ())
+        }
+
+    def __init__(self,compiler):
+        gobject.GObject.__init__(self)
+        self.compiler = compiler
+        self.queue = []
+        self.current = None
+        
+    def add(self,f,name,w,h):
+        entry = QueueEntry(copy.copy(f),name,w,h)
+        self.queue.append(entry)
+        
+    def start(self):
+        self.next()
+
+    def next(self):
+        if self.queue == []:
+            self.emit('done')
+            return
+
+        entry = self.queue.pop()
+
+        self.current = gtkfractal.HighResolution(self.compiler,entry.w,entry.h)
+        self.current.set_fractal(entry.f)
+        
+        self.current.connect('status-changed', self.onImageComplete)
+        self.current.draw_image(entry.name)
+
+    def onImageComplete(self, f, status):
+        if status == 0:
+            self.emit('image-complete')
+            self.next()
+
+# explain our existence to GTK's object system
+gobject.type_register(T)
--- fract4dgui/test_renderqueue.py
+++ fract4dgui/test_renderqueue.py
@@ -13,7 +13,11 @@ import renderqueue
 
 sys.path.append("..")
 
-from fract4d import fractal, image
+from fract4d import fractal, image, fc
+
+g_comp = fc.Compiler()
+g_comp.file_path.append("../fract4d")
+g_comp.file_path.append("../formulas")
 
 class Test(unittest.TestCase):
     def setUp(self):
@@ -22,9 +26,36 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def wait(self):
+        gtk.main()
+        
+    def quitloop(self,rq):
+        gtk.main_quit()
+
     def testRQ(self):
-        rq = renderqueue.T()
-                
+        rq = renderqueue.T(g_comp)
+        self.assertEqual(0, len(rq.queue))
+
+        # should be a no-op
+        rq.start()
+
+        # add a fractal to generate
+        f = fractal.T(g_comp)
+        rq.add(f,"rq1.png",2048,1536)
+
+        # check it got added
+        self.assertEqual(1, len(rq.queue))
+        entry = rq.queue[0]
+        self.assertEqual("rq1.png", entry.name)
+        self.assertEqual(2048, entry.w)
+        self.assertEqual(1536, entry.h)
+
+        # run
+        rq.connect('done', self.quitloop)
+        rq.start()
+        self.wait()
+
+        self.assertEqual(0, len(rq.queue))
         
 def suite():
     return unittest.makeSuite(Test,'test')
