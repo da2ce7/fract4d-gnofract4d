33a67179351cb59b5bcf1197066414069b87cfb1
Edwin Young <edwin@bathysphere.org>
2006-03-03 00:40:05 +0000

    accidentally left these out

---
+++ fract4dgui/renderqueue.py
@@ -0,0 +1,9 @@
+# A module which manages a queue of images to render in the background
+# and a UI for the same
+
+# the underlying queue object
+class T:
+    def __init__(self):
+        pass
+    
+    
