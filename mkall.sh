9170490064bcb397c8b072ec82a1a5dbfb2a93be
Edwin Young <edwin@bathysphere.org>
2006-05-06 14:21:34 +0000

    foo

--- mkall.sh
+++ mkall.sh
@@ -19,7 +19,7 @@ export MIN=2.2
 export MAX=2.3
  
 ./mkrpm.sh $BUILD_PYTHON $MIN $MAX
-mv dist/gnofract4d-3.0-1.$ARCH.rpm dist/gnofract4d-python22-3.0-1.$ARCH.rpm 
+mv dist/gnofract4d-2.14-1.$ARCH.rpm dist/gnofract4d-python22-2.14-1.$ARCH.rpm 
 
 # Python 2.3 version
 export BUILD_PYTHON_VERSION=2.3
@@ -28,7 +28,7 @@ export MIN=2.3
 export MAX=2.4
 
 ./mkrpm.sh $BUILD_PYTHON $MIN $MAX
-mv dist/gnofract4d-3.0-1.$ARCH.rpm dist/gnofract4d-python23-3.0-1.$ARCH.rpm 
+mv dist/gnofract4d-2.14-1.$ARCH.rpm dist/gnofract4d-python23-2.14-1.$ARCH.rpm 
 
 # use local gcc for Python 2.4, since that's the best match on my dev machine (FC4)
 export CC=/usr/bin/gcc
@@ -41,7 +41,7 @@ export MIN=2.4
 export MAX=2.5
 
 ./mkrpm.sh $BUILD_PYTHON $MIN $MAX
-mv dist/gnofract4d-3.0-1.$ARCH.rpm dist/gnofract4d-python24-3.0-1.$ARCH.rpm 
+mv dist/gnofract4d-2.14-1.$ARCH.rpm dist/gnofract4d-python24-2.14-1.$ARCH.rpm 
 
 unset BUILD_PYTHON_VERSION
 unset BUILD_PYTHON
@@ -53,7 +53,7 @@ unset CXX
 
 # make ISO image for testing on different OSes
 pushd dist
-mkisofs -J -R -o gf4d.iso *3.0*.gz *3.0*.rpm
+mkisofs -J -R -o gf4d.iso *2.14*.gz *2.14*.rpm
 popd dist
 
 
