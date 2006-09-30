30ee0519fc0c44c057de4705950451e7f0a9f2c6
Edwin Young <edwin@bathysphere.org>
2006-09-30 19:54:01 +0000

    yet another makemap rewrite

--- fractutils/test_makemap.py
+++ fractutils/test_makemap.py
@@ -13,21 +13,94 @@ class Test(unittest.TestCase):
 
     def testAllocate(self):
         mm = makemap.T()
-        self.assertEqual(16777216, len(mm.nodes))
 
-    def testGetIndex(self):
+        self.assertEqual(None,mm.root)
+
+    def testAddOnePixel(self):
         mm = makemap.T()
-        self.assertEqual(0x000000,mm.getIndex(0,0,0,8))
-        self.assertEqual(0x010000,mm.getIndex(1,0,0,8))
-        self.assertEqual(0x000100,mm.getIndex(0,1,0,8))
-        self.assertEqual(0x000001,mm.getIndex(0,0,1,8))
+        mm.insert_pixel(0,0,0)
+        n = mm.root
+        self.assertEqual((0,0,0,1), (n.r, n.g, n.b, n.count))
 
-        self.assertEqual(0x400308,mm.getIndex(0x40,3,8,8))
-        self.assertEqual(0xffffff,mm.getIndex(0xff,0xff,0xff,8))
+    def testAddOnePixelTwice(self):
+        mm = makemap.T()
+        mm.insert_pixel(0,0,0)
+        mm.insert_pixel(0,0,0)
+        n = mm.root
+        self.assertEqual((0,0,0,2), (n.r, n.g, n.b, n.count))
 
-        self.assertEqual(0x20 << 12 | 0x10 << 6 | 0x1 ,
-                         mm.getIndex(0x80,0x41,0x7,6))
-    
+    def testAddTwoPixels(self):
+        mm = makemap.T()
+        mm.insert_pixel(0,0,0)
+        mm.insert_pixel(0xff,0xff,0xff)
+        self.checkTwoPixels(mm)
+        
+    def testAddTwoPixelsReversed(self):
+        mm = makemap.T()
+        mm.insert_pixel(0xff,0xff,0xff)
+        mm.insert_pixel(0,0,0)
+        self.checkTwoPixels(mm)
+
+    def checkTwoPixels(self,mm):
+        n = mm.root
+        self.assertEqual((127,127,127,0), (n.r, n.g, n.b, n.count))
+        n1 = n.branches[0]
+        self.assertEqual((0,0,0,1), (n1.r, n1.g, n1.b, n1.count))
+        n2 = n.branches[7]
+        self.assertEqual((0xff,0xff,0xff,1), (n2.r, n2.g, n2.b, n2.count))
+
+    def testAddThreePixels(self):
+        mm = makemap.T()
+        mm.insert_pixel(0xff,0xff,0xff)
+        mm.insert_pixel(0,0,0)
+        mm.insert_pixel(0xff,0x00,0x00)
+        self.checkThreePixels(mm)
+
+    def checkThreePixels(self,mm):
+        self.checkTwoPixels(mm)
+        n3 = mm.root.branches[4]
+        self.assertEqual((0xff,0x00,0x00,1), (n3.r, n3.g, n3.b, n3.count))
+
+    def testAddTwoPixelsToSameBranch(self):
+        mm = makemap.T()
+        mm.insert_pixel(0xff,0xff,0xff)
+        mm.insert_pixel(0x80,0x80,0x80)
+        r = mm.root
+        self.assertEqual(False,r.isleaf())
+        b = r.branches[7]
+        self.assertEqual(False,b.isleaf())
+
+    def testAddTwoVerySimilarPixels(self):
+        mm = makemap.T()
+        mm.insert_pixel(0xff,0xff,0xff)
+        mm.insert_pixel(0xff,0xff,0xfe)
+        r = mm.root
+        depth=0
+        n = 127
+        size = 64
+        while not r.branches[7].isleaf():
+            self.assertEqual((r.r,r.g,r.b),(n,n,n))
+            n = n + size
+            size = size / 2
+            depth += 1
+            r = r.branches[7]
+        self.assertEqual(7, depth)
+        self.assertEqual(False, r.isleaf())
+        self.assertEqual((0xfe,0xfe,0xfe,0), (r.r, r.g, r.b, r.count))
+        leaf1 = r.branches[6]
+        self.assertEqual((0xff,0xff,0xfe,1),
+                         (leaf1.r, leaf1.g, leaf1.b, leaf1.count))
+        leaf2 = r.branches[7]
+        self.assertEqual((0xff,0xff,0xff,1),
+                         (leaf2.r, leaf2.g, leaf2.b, leaf2.count))
+
+        
+    def testNode(self):
+        n = makemap.Node(0,0,0,0)
+        self.assertEqual([None]*8, n.branches)
+
+        self.assertEqual(True, n.isleaf())
+        
     def testLoadPicture(self):
         'Load a very simple test image and check correctly parsed'
         mm = makemap.T()
@@ -46,42 +119,13 @@ class Test(unittest.TestCase):
             i+= 1
             
         mm.build()
-        self.assertEqual(50, mm.nodes[0])
-        self.assertEqual(50, mm.nodes[0xffffff])
-
-    def testPopularity(self):
-        mm = makemap.T()
-        mm.load(open("test001.png","rb"))
-        mm.build()
-
-        # contains 50 black, 10 white, 15 d80000, 25 ff0000 pixels
-        self.assertEqual(50, mm.nodes[0])
-        self.assertEqual(10, mm.nodes[0xffffff])
-        self.assertEqual(15, mm.nodes[0xd80000])
-        self.assertEqual(25, mm.nodes[0xff0000])
-
-        mp = mm.most_popular(3)
-        self.assertEqual([0,0xff0000, 0xd80000 ], mp)
-
-    def testChildren(self):
-        mm = makemap.T()
-        children = mm.children(0,0,0,8)
-        print ["%06x" % x for x in children]
-
-        children = mm.children(0,0,0,2)
-        print ["%06x" % x for x in children]
-
-        children = mm.children(0xfe,0xfe,0xfe,8)
-        print ["%06x" % x for x in children]
-
-    def disabled_testTatteredPopularity(self):
-        mm = makemap.T()
-        mm.load(open("tattered.jpg","rb"))
-        mm.build()
-        mp = mm.most_popular(20)
-        for c in mp:
-            (r,g,b) = (c >> 16, c >> 8 & 0xff, c & 0xff)
-            print "%d %d %d" % (r,g,b)
+        r = mm.root
+        self.assertEqual(False, r.isleaf())
+        blackNode = r.branches[0]
+        whiteNode = r.branches[7]
+        self.assertEqual(blackNode.count,50)
+        self.assertEqual(whiteNode.count,50)
+        
         
 def suite():
     return unittest.makeSuite(Test,'test')
